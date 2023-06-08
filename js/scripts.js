8c49f7082eb3cfa8738468f36fbc1c6814b9e5cc
Joseph Murray <josephwmurray@gmail.com>
2023-06-07 19:34:45 -0700

    clean up some javascript syntax

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,10 @@ function divide(number1, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const addNums = (add(number1, number2));
-const subNums = (subtract(number1, number2));
-const multNums = (multiply(number1, number2));
-const divNums = (divide(number1, number2));
+const addNums = add(number1, number2);
+const subNums = subtract(number1, number2);
+const multNums = multiply(number1, number2);
+const divNums = divide(number1, number2);
 const displayAdd = number1 + " + " + number2 + " = " + addNums + " \n";
 const displaySub = number1 + " - " + number2 + " = " + subNums + " \n";
 const displayMult = number1 + " * " + number2 + " = " + multNums + " \n";
